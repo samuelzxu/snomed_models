adbfe5b300a81283396ff60be78654836668686c
Samuel Xu <samuelzxu@gmail.com>
2024-02-24 19:45:26 +0000

    Adding more shit

---
+++ constants.py
@@ -0,0 +1,3 @@
+label2id = {"O": 0, "B-clinical_entity": 1, "I-clinical_entity": 2}
+
+id2label = {v: k for k, v in label2id.items()}
--- run1.sh
+++ run1.sh
@@ -1,6 +1,33 @@
-python cer_model_train.py --cer_model_id microsoft/deberta-v3-base
-python cer_model_train.py --cer_model_id microsoft/deberta-v3-large
-python cer_model_train.py --cer_model_id Clinical-AI-Apollo/Medical-NER
-python cer_model_train.py --cer_model_id KISTI-AI/scideberta-cs
-python cer_model_train.py --cer_model_id KISTI-AI/Scideberta-full
-python cer_model_train.py --cer_model_id KISTI-AI/scideberta
+python cer_model_train.py --cer_model_id microsoft/deberta-v3-base --use_LoRA=False --run_suffix='-noLoRA' --random_seed=10
+rm ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
+python cer_model_train.py --cer_model_id microsoft/deberta-v3-large  --use_LoRA=False --run_suffix='-noLoRA' --random_seed=10
+rm ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
+python cer_model_train.py --cer_model_id KISTI-AI/scideberta-cs  --use_LoRA=False --run_suffix='-noLoRA' --random_seed=10
+rm ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
+python cer_model_train.py --cer_model_id KISTI-AI/Scideberta-full  --use_LoRA=False --run_suffix='-noLoRA' --random_seed=10
+rm ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
+python cer_model_train.py --cer_model_id Clinical-AI-Apollo/Medical-NER  --use_LoRA=False --run_suffix='-noLoRA' --random_seed=10
+rm -rf ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
+python cer_model_train.py --cer_model_id yikuan8/Clinical-Longformer --use_LoRA=False --run_suffix='-noLoRA' --random_seed=42 --max_seq_len=4096
+rm -rf ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
+python cer_model_train.py --cer_model_id yikuan8/Clinical-BigBird --use_LoRA=True --run_suffix='-LoRA' --random_seed=42 --max_seq_len=2048
+rm -rf ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
+python cer_model_train.py --cer_model_id bvanaken/CORe-clinical-outcome-biobert-v1  --use_LoRA=False --run_suffix='-noLoRA' --random_seed=10
+rm -rf ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
+python cer_model_train.py --cer_model_id arashpcc/Bio_ClinicalBERT  --use_LoRA=False --run_suffix='-noLoRA' --random_seed=10
+rm -rf ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
+python cer_model_train.py --cer_model_id allenai/biomed_roberta_base --use_LoRA=False --run_suffix='-noLoRA' --random_seed=10
+rm -rf ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
+python cer_model_train.py --cer_model_id medicalai/ClinicalBERT --use_LoRA=False --run_suffix='-noLoRA' --random_seed=10
+rm -rf ~/.cache/wandb/artifacts/
+rm -rf ~/checkpoints
